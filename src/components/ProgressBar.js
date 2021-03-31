4d6bdb7a5739af4367e7149ca3e5cbda4ca930ef
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-03-31 13:35:24 +1300

    added animation and final commit

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,10 +1,23 @@
+import { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
-  console.log(progress, url);
 
-  return <div className='progress-bar' style={{ width: progress + '%' }}></div>;
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return (
+    <motion.div
+      className='progress-bar'
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
+  );
 };
 
 export default ProgressBar;
